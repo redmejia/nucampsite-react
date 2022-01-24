3b9737da70f56d367d51d733766bcaf45465d038
redmejia <preynaldorod@gmail.com>
2022-01-23 16:56:40 -0800

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
