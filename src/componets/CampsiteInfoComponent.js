643b53e792796da8996cdb3c019fc5f368cea253
redmejia <preynaldorod@gmail.com>
2022-02-05 09:53:39 -0800

    form and modal

--- src/componets/CampsiteInfoComponent.js
+++ src/componets/CampsiteInfoComponent.js
@@ -1,6 +1,83 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+	Button, Card, CardImg, CardText,
+	CardBody, Breadcrumb, BreadcrumbItem,
+	Modal, ModalBody, ModalHeader, Label
+} from 'reactstrap';
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from 'react-router-dom';
+
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+
+function CommentForm() {
+
+	const [isModalOpen, setModalState] = useState(false);
+
+	const toggleModal = () => setModalState(!isModalOpen);
+
+	const handleSubmit = (values) => {
+		console.log("Current state is: " + JSON.stringify(values));
+		alert("Current state is: " + JSON.stringify(values));
+	}
+
+
+	return (
+		<div>
+			<Button outline onClick={toggleModal} > <i className='fa fa-pencil fa-lg' /> Submite Comment </Button>
+			<Modal isOpen={isModalOpen} toggle={toggleModal}>
+				<ModalHeader toggle={toggleModal}>Submit Comment</ModalHeader>
+				<ModalBody>
+					<LocalForm onSubmit={values => handleSubmit(values)}>
+						<div className='form-group'>
+							<Label htmlFor="rate">Rating</Label>
+							<Control.select className='form-control' model=".rating" name='rating' id='rating'>
+								<option value="5">5</option>
+								<option value="4">4</option>
+								<option value="3">3</option>
+								<option value="2">2</option>
+								<option value="1">1</option>
+							</Control.select>
+						</div>
+						<div className='form-group'>
+							<Label htmlFor="author"> Your Name</Label>
+							<Control.text model=".author" name="author" id="author"
+								placeholder='author'
+								className='form-control'
+								validators={{
+									required,
+									minLength: minLength(2),
+									maxLength: maxLength(15)
+								}}
+
+							/>
+							<Errors
+								className="text-danger"
+								model=".author"
+								component="div"
+								messages={{
+									minLength: 'Must be at least 2 characters',
+									maxLength: 'Must be 15 characters or less'
+								}}
+							/>
+						</div>
+						<div className='form-group'>
+							<Label htmlFor="comment"> Comment</Label>
+							<Control.textarea className='form-control' rows="12" model=".text" name="text" id="text" />
+						</div>
+						<Button color="primary">Submit</Button>
+					</LocalForm>
+				</ModalBody>
+			</Modal>
+
+		</div >
+
+	)
+}
+
 
 function RenderCampsite({ campsite }) {
 	return (
@@ -26,7 +103,9 @@ function RenderComments({ comments }) {
 						<li>{comment.text}</li>
 						<li>-- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</li>
 						<p></p>
-					</div>)}
+					</div>
+					)}
+					<CommentForm />
 				</ul>
 			</div>
 		)
@@ -35,7 +114,7 @@ function RenderComments({ comments }) {
 }
 
 function CampsiteInfo(props) {
-	
+
 	if (props.campsite) {
 		return (
 			<div className="container">
