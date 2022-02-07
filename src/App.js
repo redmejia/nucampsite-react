7714daabef4fe370dd408a6016c5195686dd3f7d
redmejia <preynaldorod@gmail.com>
2022-02-06 22:26:28 -0800

    Redux Thunk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from './componets/MainComponent';
+import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
